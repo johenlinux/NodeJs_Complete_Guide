de277fcfbd60b7b33cbc10a739d234bb7b3f1347
Thet Tun <kingofwildanimals@gmail.com>
2019-08-13 10:56:23 +0630

    add handlebar template

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const expresshbs = require('express-handlebars');
+var users = [];
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expresshbs({ defaultLayout: 'main-layout', extname: 'hbs' }));
+
+app.use(bodyParser.urlencoded( {extended: false} ));
+
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.get('/', (req, res, next) => {
-    res.render('index', {pageTitle: 'Add Product'});
+    res.render('index', {
+        pageTitle: 'Users', 
+        users: users,
+        hasUsers: users.length >0
+    });
 });
 
-// app.get('/users', (req, res, next) => {
+app.get('/users', (req, res, next) => {
+    res.render('users', {pageTitle: 'Add Users'})
+});
 
-// });
-
-// app.post('/add-user', (req, res, next) => {
-
-// });
+app.post('/add-users', (req, res, next) => {
+    users.push({name: req.body.title});
+    res.redirect('/');
+});
 
 app.listen(8000);
